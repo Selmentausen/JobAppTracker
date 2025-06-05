9aabb74d445ce7c80203e961871b837b352e8e55
Ibrahim <Selmentausen@gmail.com>
2025-06-05 14:25:08 +0300

    create project skeleteon and base apps

--- JobAppTracker/settings.py
+++ JobAppTracker/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my apps
+    "accounts",
+    "applications",
+    "contacts",
+    "tasks",
+    "core",
 ]
 
 MIDDLEWARE = [
